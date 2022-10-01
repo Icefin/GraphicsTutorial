c9716debb6c1ccc8a7497b3f2fbf82e722b1b09e
GeonHo Shin <76864202+Icefin@users.noreply.github.com>
2022-10-01 18:28:46 +0900

    Initial commit

---
+++ GraphicsTutorial/MainGame.cpp
@@ -0,0 +1,87 @@
+#include "MainGame.h"
+#include <iostream>
+
+void FatalError(std::string errorString) {
+	std::cout << errorString << '\n';
+	std::cout << "Enter any key to quit...";
+	int tmp;
+	std::cin >> tmp;
+	SDL_Quit();
+}
+
+MainGame::MainGame() {
+	_window = nullptr;
+	_screenWidth = 1024;
+	_screenHeight = 768;
+	_gameState = GameState::PLAY;
+}
+MainGame::~MainGame() {
+
+}
+
+void MainGame::Run() {
+	InitSystems();
+	
+	GameLoop();
+
+}
+
+void MainGame::InitSystems() {
+	//Initialize SDL
+	SDL_Init(SDL_INIT_EVERYTHING);
+	//Create Window
+	_window = SDL_CreateWindow("Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+								_screenWidth, _screenHeight, SDL_WINDOW_OPENGL);
+	if (_window == nullptr) {
+		FatalError("SDL Window could not be created");
+	}
+
+	SDL_GLContext glContext = SDL_GL_CreateContext(_window);
+	if (glContext == nullptr) {
+		FatalError("SDL_GLContext could not be created");
+	}
+
+	GLenum error = glewInit();
+	if (error != GLEW_OK) {
+		FatalError("Could not initialize glew");
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void MainGame::GameLoop() {
+	while (_gameState != GameState::EXIT){
+		ProcessInput();
+		DrawGame();
+	}
+}
+void MainGame::ProcessInput() {
+	SDL_Event evnt;
+
+	while (SDL_PollEvent(&evnt)) {
+		switch (evnt.type) {
+			case SDL_QUIT :
+				_gameState = GameState::EXIT;
+				break;
+			case SDL_MOUSEMOTION :
+				std::cout << evnt.motion.x << " " << evnt.motion.y << '\n';
+				break;
+		}
+	}
+}
+
+void MainGame::DrawGame() {
+	glClearDepth(1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glEnableClientState(GL_COLOR_ARRAY);
+	glBegin(GL_TRIANGLES);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		glVertex2f(0.0f, 0.0f);
+		glVertex2f(0.0f, 100.0f);
+		glVertex2f(200.0f, 100.0f);
+	glEnd();
+
+	SDL_GL_SwapWindow(_window);
+}
---
+++ GraphicsTutorial/MainGame.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SDL/SDL.h>
+#include <GL/glew.h>
+
+enum class GameState { PLAY, EXIT };
+
+class MainGame
+{
+public :
+	MainGame();
+	~MainGame();
+	void Run();
+
+private :
+	void InitSystems();
+	void GameLoop();
+	void ProcessInput();
+	void DrawGame();
+	SDL_Window* _window;
+
+	int _screenWidth;
+	int _screenHeight;
+	GameState _gameState;
+};
